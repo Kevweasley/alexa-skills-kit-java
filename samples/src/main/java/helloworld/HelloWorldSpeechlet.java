8776fef0ee7b6fd3a94223323d74e90fea6a38e7
kenpaulsen <kenapaulsen@gmail.com>
2016-08-14 18:45:41 -0700

    Added number guessing stuff... doesn't work right, though.

--- samples/src/main/java/helloworld/HelloWorldSpeechlet.java
+++ samples/src/main/java/helloworld/HelloWorldSpeechlet.java
@@ -9,6 +9,7 @@
  */
 package helloworld;
 
+import com.amazon.speech.ui.OutputSpeech;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,20 +26,26 @@ import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 
+import java.util.Random;
+
 /**
  * This sample shows how to create a simple speechlet for handling speechlet requests.
  */
 public class HelloWorldSpeechlet implements Speechlet {
     private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
+    private static final Random rand = new Random();
 
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
             throws SpeechletException {
         log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
                 session.getSessionId());
-        // any initialization logic goes here
+        session.setAttribute("number", rand.nextInt(100) + 1);
     }
 
+    /**
+     * Asked when "open ..." is said. Or when "ask ..." w/o anything after is said.
+     */
     @Override
     public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
             throws SpeechletException {
@@ -57,7 +64,9 @@ public class HelloWorldSpeechlet implements Speechlet {
         String intentName = (intent != null) ? intent.getName() : null;
 
         if ("HelloWorldIntent".equals(intentName)) {
-            return getHelloResponse();
+            return getHelloResponse(request, session);
+        } if ("GuessNumberIntent".equals(intentName)) {
+            return getGuessNumberResponse(request, session);
         } else if ("AMAZON.HelpIntent".equals(intentName)) {
             return getHelpResponse();
         } else {
@@ -97,13 +106,60 @@ public class HelloWorldSpeechlet implements Speechlet {
         return SpeechletResponse.newAskResponse(speech, reprompt, card);
     }
 
+    private SpeechletResponse getGuessNumberResponse(final IntentRequest request, final Session session) {
+        // Get the answer
+        int answer = (Integer) session.getAttribute("number");
+
+        // Get the guess
+        String guessStr = request.getIntent().getSlot("guess").getValue();
+        int guess = 0;
+        if (guessStr != null) {
+            guess = Integer.valueOf(guessStr);
+        }
+
+        SpeechletResponse resp;
+        if (guess == answer) {
+            String speechText = "Congratulations!!  You guessed it!";
+
+            // Create the Simple card content.
+            SimpleCard card = new SimpleCard();
+            card.setTitle("Number Guess");
+            card.setContent(speechText);
+
+            resp = SpeechletResponse.newTellResponse(getSpeech(speechText), card);
+        } else {
+            OutputSpeech errorMessage;
+            Reprompt prompt = new Reprompt();
+            prompt.setOutputSpeech(getSpeech("Guess again."));
+            if (guess < answer) {
+                errorMessage = getSpeech("Sorry... too low!");
+            } else {
+                errorMessage = getSpeech("Sorry... too high!");
+            }
+            resp = SpeechletResponse.newAskResponse(errorMessage, prompt);
+            resp.setShouldEndSession(false);
+        }
+        log.info("NUMBER GUESS: {} was guessed; {} was the answer; should continue? {}",
+                guess, answer, resp.getShouldEndSession());
+
+        return resp;
+    }
+
+    private OutputSpeech getSpeech(String str) {
+        PlainTextOutputSpeech out = new PlainTextOutputSpeech();
+        out.setText(str);
+        return out;
+    }
+
     /**
      * Creates a {@code SpeechletResponse} for the hello intent.
      *
      * @return SpeechletResponse spoken and visual response for the given intent
      */
-    private SpeechletResponse getHelloResponse() {
-        String speechText = "Hello world";
+    private SpeechletResponse getHelloResponse(final IntentRequest request, final Session session) {
+        String speechText = "Hello " + session.getUser().getUserId();
+        //request.getIntent().getSlot("foo");
+
 
         // Create the Simple card content.
         SimpleCard card = new SimpleCard();
